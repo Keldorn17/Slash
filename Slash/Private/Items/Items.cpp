4e6391aa63f4485ec4d9e380984155a101524a97
Zoltán Patai <113589766+Keldorn17@users.noreply.github.com>
2023-03-13 10:53:49 +0100

    3/13/2023

--- Slash/Private/Items/Items.cpp
+++ Slash/Private/Items/Items.cpp
@@ -1,6 +1,6 @@
 
 
-#include "Items/Item.h"
+#include "Items/Items.h"
 #include "Slash/DebugMacros.h"
 
 // #define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 15.f, 20, FColor::Red, true); 
@@ -8,42 +8,37 @@
 // If I define something (called Macro) before compiling the program will replace it with the actual line of code so semicolon is not required on call 
 // Example DRAW_SPHERE(Location)
 
-AItem::AItem()
+AItems::AItems()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
 }
 
-void AItem::BeginPlay()
+void AItems::BeginPlay()
 {
 	Super::BeginPlay();
-
+	
 }
-
-float AItem::TransformedSin()
+float AItems::TransformedSin()
 {
 	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
 }
-float AItem::TransformedCos()
+float AItems::TransformedCos()
 {
 	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
+void AItems::RotateObject()
+{
+	SetActorRotation(FRotator(0.f, 30.f, 0.f) * RunningTime);
+}
 
-void AItem::Tick(float DeltaTime)
+void AItems::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 	RunningTime += DeltaTime;
-
-	// float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
-
-	// AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
-	
-	DRAW_SPHERE_SINGLEFRAME(GetActorLocation());
-	DRAW_VECTOR_SINGLEFRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100);
-
-	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
-	DRAW_POINT_SINGLEFRAME(AvgVector);
 
 }
 
